82f600780814391cb871dbad77a7c4a5a052757e
Hayoung-Im <143571041+Hayoung-Im@users.noreply.github.com>
2024-02-02 21:27:12 +0900

    Update alarmFunctions.js

--- src/functions/alarmFunctions.js
+++ src/functions/alarmFunctions.js
@@ -6,49 +6,58 @@ const schedule = require('node-schedule');
 
 admin.initializeApp();
 
-// Firestore 'alarms' 컬렉션에 문서가 생성될 때 실행되는 함수
 exports.scheduleNotifications = functions.firestore.document('alarms/{alarmId}')
   .onCreate(async (snapshot, context) => {
-    // 알람 데이터 가져오기
-    const alarmData = snapshot.data();
+    try {
+      const alarmData = snapshot.data();
+      const { days, time, userId } = alarmData;
+      const [hour, minute] = time.split(':');
+      
+      const scheduledDate = new Date();
+      scheduledDate.setUTCHours(Number(hour));
+      scheduledDate.setUTCMinutes(Number(minute));
+      const dayOfWeek = days.map(day => ['일', '월', '화', '수', '목', '금', '토'].indexOf(day));
+      
+      const rule = new schedule.RecurrenceRule();
+      rule.dayOfWeek = dayOfWeek;
+      rule.hour = scheduledDate.getUTCHours();
+      rule.minute = scheduledDate.getUTCMinutes();
 
-    // 푸시 알림을 예약할 날짜 및 시간 설정
-    const { days, time, userId } = alarmData;
-    const [hour, minute] = time.split(':');
-    const scheduledDate = new Date();
-    scheduledDate.setUTCHours(Number(hour));
-    scheduledDate.setUTCMinutes(Number(minute));
-    const dayOfWeek = days.map(day => ['일', '월', '화', '수', '목', '금', '토'].indexOf(day));
-    
-    // 예약 조건 설정 (요일, 시간)
-    const rule = new schedule.RecurrenceRule();
-    rule.dayOfWeek = dayOfWeek;
-    rule.hour = scheduledDate.getUTCHours();
-    rule.minute = scheduledDate.getUTCMinutes();
+      const job = schedule.scheduleJob(rule, async () => {
+        try {
+          const userSnapshot = await admin.firestore().collection('users').doc(userId).get();
+          const user = userSnapshot.data();
+          const userFCMToken = user.fcmToken;
 
-    // 예약된 작업 생성
-    const job = schedule.scheduleJob(rule, async () => {
-      try {
-        // 알림 메시지 구성
-        const message = {
-          notification: {
-            title: 'Truffle',
-            body: '지금까지의 소비를 확인해보세요',
-          },
-        };
-        
-        // 사용자의 FCM 토큰 가져오기
-        const userSnapshot = await admin.firestore().collection('users').doc(userId).get();
-        const user = userSnapshot.data();
-        const userFCMToken = user.fcmToken;
+          const message = {
+            notification: {
+              title: 'Truffle',
+              body: '지금까지의 소비를 확인해보세요',
+            },
+            token: userFCMToken,
+            android: {
+              priority: 'high',
+              notification: {
+                click_action: 'YOUR_ACTION_ACTIVITY', // 여기 부분에 그 알람 푸쉬 누르면 어디로 갈지 결정해서 설정 해야함.
+              },
+            },
+          };
 
-        // FCM을 통해 알림 메시지 전송
-        const response = await admin.messaging().sendToDevice(userFCMToken, message);
-        console.log('알림이 성공적으로 전송되었습니다:', response);
-      } catch (error) {
-        console.error('알림 전송 중 오류 발생:', error);
-      }
-    });
+          const response = await admin.messaging().send(message);
+          console.log('알림이 성공적으로 전송되었습니다:', response);
+        } catch (error) {
+          console.error('알림 전송 중 오류 발생:', error);
+        }
+      });
+
+      console.log('알람이 예약되었습니다.');
+    } catch (error) {
+      console.error('알람 예약 중 오류 발생:', error);
+    }
+
+    return null;
+  });
+
 
     console.log('알람이 예약되었습니다.');
 
